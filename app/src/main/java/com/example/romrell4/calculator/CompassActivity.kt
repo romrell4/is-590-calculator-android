3990249b79d4854a623b4e8e39d188dac42f3019
Eric Romrell <eric_romrell@byu.edu>
2018-01-29 11:51:56 -0700

    Added all recycler view code... Still not working... :-/

--- app/src/main/java/com/example/romrell4/calculator/CompassActivity.kt
+++ app/src/main/java/com/example/romrell4/calculator/CompassActivity.kt
@@ -1,36 +1,35 @@
 package com.example.romrell4.calculator
 
 import android.Manifest
-import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
-import android.widget.TextView
-import com.google.android.gms.location.*
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import kotlinx.android.synthetic.main.activity_compass2.*
+import kotlinx.android.synthetic.main.list_item.*
 
 class CompassActivity : AppCompatActivity() {
-    private var latitude: TextView? = null
-    private var longitude: TextView? = null
-    private var altitude: TextView? = null
-    private var accuracy: TextView? = null
-    private var speed: TextView? = null
-    private var provider: TextView? = null
+    private lateinit var adapter: InfoRecyclerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_compass)
+        setContentView(R.layout.activity_compass2)
 
-        latitude = findViewById(R.id.latitude)
-        longitude = findViewById(R.id.longitude)
-        altitude = findViewById(R.id.altitude)
-        accuracy = findViewById(R.id.accuracy)
-        speed = findViewById(R.id.speed)
-        provider = findViewById(R.id.provider)
+        val list = listOf(Pair("Latitude", "Test"))
+        adapter = InfoRecyclerAdapter(list)
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-            && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        recyclerView.adapter = adapter
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
         }
 
@@ -47,12 +46,42 @@ class CompassActivity : AppCompatActivity() {
     }
 
     fun onLocationChanged(location: Location?) {
-        latitude?.text = location?.latitude.toString()
-        longitude?.text = location?.longitude.toString()
-        altitude?.text = getString(R.string.altitude_format, location?.altitude.toString())
-        accuracy?.text = getString(R.string.accuracy_format, location?.accuracy.toString())
-        speed?.text = getString(R.string.speed_format, location?.speed.toString())
-        provider?.text = location?.provider
+        adapter.list = listOf(
+                Pair("Latitude", location?.latitude.toString()),
+                Pair("Longitude", location?.longitude.toString())
+        )
+
+//        latitude.text = location?.latitude.toString()
+//        longitude.text = location?.longitude.toString()
+//        altitude.text = getString(R.string.altitude_format, location?.altitude.toString())
+//        accuracy.text = getString(R.string.accuracy_format, location?.accuracy.toString())
+//        speed.text = getString(R.string.speed_format, location?.speed.toString())
+//        provider.text = location?.provider
     }
 
+    inner class InfoRecyclerAdapter(list: List<Pair<String, String>>) : RecyclerView.Adapter<InfoRecyclerAdapter.InfoViewHolder>() {
+        var list: List<Pair<String, String>> = list
+            set(value) {
+                field = value
+                notifyDataSetChanged()
+            }
+
+        override fun getItemCount() = list.size
+
+        override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): InfoViewHolder {
+            return InfoViewHolder(LayoutInflater.from(parent?.context).inflate(R.layout.list_item, parent, false))
+        }
+
+        override fun onBindViewHolder(holder: InfoViewHolder?, position: Int) {
+            holder?.bind(list.get(position))
+        }
+
+        inner class InfoViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+            fun bind(pair: Pair<String, String>) {
+                textView.text = pair.first
+                detailTextView.text = pair.second
+            }
+        }
+    }
 }
